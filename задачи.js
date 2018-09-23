07da13830fbd36b154c264d0d17c46045e81034e
Dan <wrf.life@gmail.com>
2018-09-23 21:07:58 +0300

    Добавлено решение 2 задач и оптимизирован внешний вид сайта.

--- задачи.js
+++ задачи.js
@@ -3,6 +3,16 @@ function задача1(){
    const d = $('#d').val();
    const e = $('#e').val();
    const e0 = 8.85 * Math.pow(10,-12);
-   let w = e0*e*Math.pow(u,2)/(2*Math.pow(d,2));
-   alert("Result is = " + w + " Дж/м3");
+   const w = e0*e*Math.pow(u,2)/(2*Math.pow(d,2));
+   alert("Вiдповiдь: ω = " + w + " Дж/м3");
+}
+
+function задача2() {
+    const c = $('#c').val();
+    const u = $('#u2').val();
+    const t = $('#t').val();
+    const w = c*Math.pow(u,2)/2;
+    const p = w/t;
+    alert("Вiдповiдь: W = " + w + "Дж" +
+        "\n P = " + Math.round(p) + "Вт");
 }
